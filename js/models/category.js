ff0de7dc56e22cf0acd1db3064c7c43be3bf0178
jeremylenz <jeremylenz@gmail.com>
2017-08-01 17:58:07 -0400

    users and categories adapter

--- js/models/category.js
+++ js/models/category.js
@@ -1,14 +1,26 @@
 function createCategories () {
-  let id = 0
+
 
   return class {
-    constructor (name, image_url) {
+    constructor (name, image_url, id) {
     this.name = name
     this.image_url = image_url
-    this.id = ++id
+    this.id = id
     store.categories.push(this)
     }
-  }
+
+    static find(id) {
+      return store.categories.find((category) => {
+        return category.id === id
+      })
+    }
+
+    static createFromApi(categoryData) {
+      return new Category(categoryData.name, categoryData.image_url, categoryData.id)
+    }
+
+
+  } //end of class
 
 }
 
