a8518900e7468eb9e1d1c25007ed5d8d8846d348
github username <github email address>
2017-08-01 13:54:21 -0400

    added session model and added sessions and current_user to store

--- js/models/session.js
+++ js/models/session.js
@@ -4,9 +4,23 @@ function createSession(){
   return class {
     constructor(user_id){
       this.user_id = user_id
-      this.user = fetch(`http://localhost:3000/users/${user_id}`)
-        .then(respones => response.json())
-        .then(json => )
+      this.username = fetch(`http://localhost:3000/users/${user_id}`)
+        .then(respones => return response.json().username)
+      this.startTime = Date.now
+      this.endTime = null
+      store.sessions.push(this)
+      store.current_user = this.username
     };
+
+    static logOut(){
+      let index = store.sessions.findIndex(function(session){
+        return session.username === store.current_user
+      })
+      let session = store.sessions[index]
+      session.endtime = Date.now
+      store.current_user = null
+    }
   }
 }
+
+let Session = createSession()
